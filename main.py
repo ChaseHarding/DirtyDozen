648d3e910b029e978ba129c7a809289605a4418d
ChaseHarding <chaseharding965@gmail.com>
2024-08-03 11:46:15 -0700

    left information about project and set up initial list

---
+++ main.py
@@ -0,0 +1,8 @@
+# Hello, Below is a list of the top 12 produce items that found the
+#  highest amounts of pesticides attached to them after reasearch according to an articled
+# posted by delish in 2019, found here: https://www.delish.com/food-news/a26872638/dirty-dozen-foods-list-2019/
+
+#This is simply for study purposes to understanding list items in python.
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches",
+                "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
